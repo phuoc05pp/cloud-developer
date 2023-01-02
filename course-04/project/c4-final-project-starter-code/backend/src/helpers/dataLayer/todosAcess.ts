20dae867e5c52642060e429a596a59f89672a53d
PhanHuuPhuoc <phuoc05pp@gmail.com>
2023-01-03 01:16:57 +0700

    refactor

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/dataLayer/todosAcess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/dataLayer/todosAcess.ts
@@ -1,9 +1,9 @@
 import * as AWS from 'aws-sdk'
 const AWSXRay = require('aws-xray-sdk');
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-import { createLogger } from '../utils/logger'
-import { TodoItem } from '../models/TodoItem'
-import { TodoUpdate } from '../models/TodoUpdate';
+import { createLogger } from '../../utils/logger'
+import { TodoItem } from '../../models/TodoItem'
+import { TodoUpdate } from '../../models/TodoUpdate';
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
